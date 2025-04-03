973a2e268efd3a0e91df095c76aeae6fea5bf625
Trisha Barua <trishabarua308@gmail.com>
2025-04-04 01:52:05 +0600

    updated

--- grade.py
+++ grade.py
@@ -5,3 +5,12 @@ elif 70<=m<80:  # or , take 70 also a+,so use and
     print("A")
 elif 60<=m<70:
     print("A-")
+
+i=1
+while i<=10:
+    if i==5:
+        break
+    print(i,". Trisha")
+    i= i+1
+
+print("Stop")
