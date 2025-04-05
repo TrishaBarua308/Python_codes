47e35dc1f25c7712cdcf9ebd3c55435509dc6acb
Trisha Barua <trishabarua308@gmail.com>
2025-04-05 22:01:51 +0600

    range

---
+++ range.py
@@ -0,0 +1,9 @@
+pi = list(range(10))
+print(pi)
+print(pi[3]) #print 3 no index
+
+num = list(range(5,11))  # before last index
+print(num)
+
+num = list(range(10,101,10)) # last was interval
+print(num)
