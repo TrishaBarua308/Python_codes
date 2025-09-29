05fa6549e0e3e3bc7292322ee198caf69d0ba7c7
trishabarua308@gmail.com <trishabarua308@gmail.com>
2025-09-30 00:38:57 +0600

    user define

---
+++ user_defined_func.py
@@ -0,0 +1,31 @@
+print("no input no return")
+def my():
+    a=10
+    b=20
+    print(a+b)
+
+my()
+
+print("\n------------------------------\n")
+print("Input, no return")
+
+def my_func(a,b):
+    print(a+b)
+
+my_func(5,10)
+
+print("\n------------------------------\n")
+print("Input, return")
+
+def new_func(a,b):
+    return a*b
+
+print(new_func(4,8))
+
+print("\n------------------------------\n")
+print("no Input, return")
+
+def h():
+    return "Hello Trisha"
+
+print(h())
