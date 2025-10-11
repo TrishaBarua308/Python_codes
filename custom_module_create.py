b5f9da9f0a877b0dd7a1cc7f052de00debaf514a
Trisha Barua <152303377+TrishaBarua308@users.noreply.github.com>
2025-10-12 00:58:26 +0600

    Add files via upload

---
+++ custom_module_create.py
@@ -0,0 +1,10 @@
+# import custom
+# print(custom.add(4,5))
+# print(custom.sub(4,5))
+# print(custom.multi(4,5))
+# print(custom.division(4,5))
+# print(custom.mod(4,5))
+
+import custom as c
+print(c.add(6,5))
+print(c.multi(6,5))
