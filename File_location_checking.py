3f2a405a8e9f71ca66fd0acc8153967b48daddc6
Trisha Barua <152303377+TrishaBarua308@users.noreply.github.com>
2025-10-12 22:11:09 +0600

    Add files via upload

---
+++ File_location_checking.py
@@ -0,0 +1,8 @@
+print('\n')
+import file_location_test as f
+print(f.add(8,9))
+print(f.multi(6,5))
+# print(__name__)  # at 1st print file_location_test then main,
+#  it indicates that those code is imported by another
+
+
---
+++ file_location_test.py
@@ -0,0 +1,10 @@
+
+
+
+# print(__name__)
+
+if __name__ == '__main__':
+    def add(a, b): return a + b
+    def multi(a, b): return a * b
+    def sub(a, b): return a - b
+    print("\nFile only executable directly from the actual file not others")
