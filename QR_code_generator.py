2f2d70baabc20be8d7474cd7955f38557a04409b
Trisha Barua <152303377+TrishaBarua308@users.noreply.github.com>
2025-10-15 23:54:53 +0600

    Add files via upload

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -1,29 +1,40 @@
 '''import qrcode
 
-text = input("Input text to convert it into qrcode:")
-filename = input("Input filename to save qr code in image :")
+text=input("Input text to convert it into a qr code:")
+filename=input("Input a filename to save the qr code image:")
 
-def generate_qr_code(text, filename):
-    image_qr_code = qrcode.make(text)
-    image_qr_code.save(filename)
+def generate_qr_code(text,filename):
+    img_qrcode=qrcode.make(text)
+    img_qrcode.save(filename)
 
-generate_qr_code(text,filename)
+generate_qr_code(text,filename)'''
 
-
-'''
-
-import qrcode
-def generate_qr_code(filepath):
-    with open(filepath,'r') as f:
-        lines = f.readlines()
-    text = lines[0].strip()
-    filename = lines[1].strip()
-
-    # qr make
-    image_qr_code = qrcode.make(text)
-
-    # qr image save
-    image_qr_code.save(filename)
+# import qrcode
+#
+# def generate_qr(filepath):
+#     with open(filepath, 'r') as f:
+#         lines = f.readlines()
+#     text = lines[0].strip()
+#     filename = lines[1].strip()
+#
+#     img_qr = qrcode.make(text)
+#     img_qr.save(filename)
+#
+# generate_qr('input.txt')
 
 
-generate_qr_code('input.txt')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
